6699672c51c40668edca8acc8f099d7d97471ad4
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-08-23 16:30:06 -0300

    [done] searching for subvetor inside a vector

--- assembly/subvector.asm
+++ assembly/subvector.asm
@@ -1,35 +1,32 @@
 .section .data
-    I: .quad 0  # %rdi
-    J: .quad 0  # %rsi
-    VETOR: .quad 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 # %rax
+    VETOR: .quad 1, 6, 7, 4, 5, 6, 7, 8, 9, 10 # %rax
     SUBVETOR: .quad 6, 7, 8    # %rbx
-    HAS_SUB: .quad 0 # %rcx
 .section .text
 .globl _start
 _start:
-    movq $0, I
-    movq $0, J
-    movq $0, HAS_SUB
-
+    movq $0, %rdi    # I
+    movq $0, %rsi    # J
+    movq $0, %rcx    # I temp
 main_loop:
-    movq I, %rdi    # rdi=0
-    cmpq $10, %rdi  # verifica fim do loop
+    movq %rcx, %rdi # retorna o valor anterior de I
+    cmpq $9, %rdi  # verifica fim do loop
     je fim_err     # não tem o subvetor
-    movq J, %rsi    # rsi=0
     movq VETOR(, %rdi, 8), %rax     # salva vetor[i]
     movq SUBVETOR(, %rsi, 8), %rbx # salva subvetor[j]
+    movq %rdi, %rcx # salva variável temporária, para caso não encontre o subvetor
     cmpq %rbx, %rax # compara vetor[i] e subvetor[j]
     je sub_loop    # pula pro outro laco se forem iguais
-    addq $1, %rdi   # soma i
-    movq %rdi, I    # salva em I
+    addq $1, %rcx   # soma i
     jmp main_loop   # proximo loop
 sub_loop:
     addq $1, %rdi   # i++
     addq $1, %rsi   # j++
-    cmpq $4, %rdi   # confere fim do subloop
+    cmpq $3, %rsi   # confere fim do subloop
     je fim_succ    # tem subvetor
+    cmpq $9, %rdi   # confere fim do loop principal
+    je fim_err      # não tem o subvetor
     movq VETOR(, %rdi, 8), %rax     # salva vetor[i] em rax
-    movq SUBVETOR(, %rdi, 8), %rbx  # salva subvetor[j] em rbx
+    movq SUBVETOR(, %rsi, 8), %rbx  # salva subvetor[j] em rbx
     cmpq %rbx, %rax # compara vetor[i] e subvetor[j]
     jne main_loop   # volta pro loop principal
     jmp sub_loop    # proxima iteracao
